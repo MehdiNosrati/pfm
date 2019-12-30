773ad15b67912f89e81defeb4509754bdd77e94f
Mehdi Nosrati Sahlan <mehdins@aut.ac.ir>
2019-12-30 21:36:10 +0330

    refactor home

--- app/src/main/java/io/mns/mpfm/ui/fragments/HomeFragment.java
+++ app/src/main/java/io/mns/mpfm/ui/fragments/HomeFragment.java
@@ -1,4 +1,4 @@
-package io.mns.mpfm.ui;
+package io.mns.mpfm.ui.fragments;
 
 
 import android.os.Bundle;
@@ -10,16 +10,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
-import androidx.recyclerview.widget.LinearLayoutManager;
-
-import java.util.List;
+import androidx.navigation.Navigation;
 
 import io.mns.mpfm.R;
 import io.mns.mpfm.databinding.FragmentHomeBinding;
-import io.mns.mpfm.db.entities.Transaction;
 import io.mns.mpfm.ui.adapters.TransactionAdapter;
+import io.mns.mpfm.ui.callbacks.TransactionClickCallback;
 import io.mns.mpfm.viewmodels.HomeViewModel;
 
 
@@ -29,14 +26,13 @@ import io.mns.mpfm.viewmodels.HomeViewModel;
 public class HomeFragment extends Fragment implements TransactionClickCallback {
 
     private FragmentHomeBinding binding;
-    private HomeViewModel viewmodel;
+    private HomeViewModel viewModel;
     private TransactionAdapter adapter;
 
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false);
         return binding.getRoot();
     }
@@ -45,19 +41,31 @@ public class HomeFragment extends Fragment implements TransactionClickCallback {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        HomeViewModel.Factory factory = new HomeViewModel.Factory(getActivity().getApplication());
-        viewmodel = ViewModelProviders.of(this, factory).get(HomeViewModel.class);
-        binding.setViewmodel(viewmodel);
-        adapter = new TransactionAdapter(this);
-        binding.transactionList.setAdapter(adapter);
-        viewmodel.loadTransactions().observe(this, transactions -> {
-            adapter.setData(transactions);
-        });
+        setupViewModel();
+        setupListeners();
+        setupTransactionList();
+        startDataObservation();
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
+    private void startDataObservation() {
+        viewModel.loadTransactions().observe(this, transactions ->
+                adapter.setData(transactions));
+    }
+
+    private void setupTransactionList() {
+        adapter = new TransactionAdapter(this);
+        binding.transactionList.setAdapter(adapter);
+    }
+
+    private void setupListeners() {
+        binding.newTransaction.setOnClickListener(v ->
+                Navigation.findNavController(v).navigate(R.id.home_to_add_transaction));
+    }
+
+    private void setupViewModel() {
+        HomeViewModel.Factory factory = new HomeViewModel.Factory(getActivity().getApplication());
+        viewModel = ViewModelProviders.of(this, factory).get(HomeViewModel.class);
+        binding.setViewmodel(viewModel);
     }
 
     @Override
--- app/src/main/java/io/mns/mpfm/viewmodels/HomeViewModel.kt
+++ app/src/main/java/io/mns/mpfm/viewmodels/HomeViewModel.kt
@@ -1,23 +1,16 @@
 package io.mns.mpfm.viewmodels
 
 import android.app.Application
-import android.view.View
 import androidx.annotation.NonNull
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.Navigation
 import io.mns.mpfm.DataRepository
 import io.mns.mpfm.PfmApplication
-import io.mns.mpfm.R
 import io.mns.mpfm.db.entities.Transaction
 
 class HomeViewModel(application: Application, private val dataRepository: DataRepository) : AndroidViewModel(application) {
-
-    fun navigateToTransaction(v: View?) {
-        Navigation.findNavController(v!!).navigate(R.id.home_to_add_transaction)
-    }
 
     fun loadTransactions(): LiveData<List<Transaction>> {
         return dataRepository.transactions
