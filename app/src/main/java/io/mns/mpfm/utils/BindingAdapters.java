cb0206ae6de7713e5d6b0ce687b374409f8d5d8f
Mehdi Nosrati Sahlan <mehdins@aut.ac.ir>
2020-01-02 20:33:00 +0330

    add binding adapters

---
+++ app/src/main/java/io/mns/mpfm/utils/BindingAdapters.java
@@ -0,0 +1,12 @@
+package io.mns.mpfm.utils;
+
+import android.view.View;
+
+import androidx.databinding.BindingAdapter;
+
+public class BindingAdapters {
+    @BindingAdapter("visibleGone")
+    public static void showHide(View view, boolean show) {
+        view.setVisibility(show ? View.VISIBLE : View.GONE);
+    }
+}
