250610176bb48e42ccf0213215c66de26893289b
Mehdi Nosrati Sahlan <mehdins@aut.ac.ir>
2020-01-03 16:39:43 +0330

    add background setting binding adapter

--- app/src/main/java/io/mns/mpfm/utils/BindingAdapters.java
+++ app/src/main/java/io/mns/mpfm/utils/BindingAdapters.java
@@ -1,5 +1,6 @@
 package io.mns.mpfm.utils;
 
+import android.graphics.Color;
 import android.view.View;
 
 import androidx.databinding.BindingAdapter;
@@ -8,5 +9,10 @@ public class BindingAdapters {
     @BindingAdapter("visibleGone")
     public static void showHide(View view, boolean show) {
         view.setVisibility(show ? View.VISIBLE : View.GONE);
+    }
+
+    @BindingAdapter("backgroundString")
+    public static void setBackground(View v, String backgorund) {
+        v.setBackgroundColor(Color.parseColor(backgorund));
     }
 }
